1ae340cb1ef562cb6f5ba2c20bd5a0ee791de895
RohanMeduri <86210535+RohanMeduri@users.noreply.github.com>
2022-10-26 23:09:03 -0700

    Add files via upload

---
+++ quiz_name_login.js
@@ -0,0 +1,18 @@
+function getUserName(){
+    localStorage.getItem(player1_name);
+    localStorage.getItem(player2_name);
+    currentPlayer1 = player1_name;
+    currentPlayer2 = player2_name;
+    document.getElementById("Question").innerHTML = currentPlayer1;
+
+}
+
+function addUser(){
+    player1_name = document.getElementById("player1_name_input").value;
+    player2_name = document.getElementById("player2_name_input").value;
+
+    localStorage.setItem("player1_name", player1_name);
+    localStorage.setItem("player2_name", player2_name);
+
+    window.location = "quiz_game_page.html";
+}
